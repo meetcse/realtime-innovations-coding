9a39d64ef41e81885d7b75115579ddf112ad113f
Meet Prajapati <52591248+meetcse@users.noreply.github.com>
2025-02-05 20:41:45 +0530

    feat : Upgraded to new flutter version

--- android/app/src/main/kotlin/com/example/realtime_innovations_coding/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/realtime_innovations_coding/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.realtime_innovations_coding
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
